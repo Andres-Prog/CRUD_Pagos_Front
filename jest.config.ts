b349482e59a667d719ffe20e9e42720ebea5d2e1
Andres <a.pacheco.lab@gmail.com>
2025-07-10 11:23:36 -0300

    config coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,26 +2,47 @@ import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  // Proporciona la ruta a tu aplicación Next.js para cargar next.config.js y .env en el entorno de prueba
   dir: './',
 });
 
-// Añade cualquier configuración personalizada de Jest que desees
 const config: Config = {
-  coverageProvider: 'v8',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  
-  // --- CORRECCIÓN ---
-  // Esta configuración le dice a Jest cómo resolver los alias de ruta de tu tsconfig.json.
-  // Asume que tu alias '@/' apunta a 'src/app/'.
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/src/app/components/$1',
     '^@/services/(.*)$': '<rootDir>/src/app/services/$1',
     '^@/types$': '<rootDir>/src/app/types/index',
     '^@/context/(.*)$': '<rootDir>/src/app/context/$1',
   },
+
+  collectCoverage: true, 
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage', // La carpeta donde se guardarán los reportes
+
+  // Define de qué archivos queremos medir la cobertura. Es importante excluir
+  // archivos de configuración, layouts, páginas principales, tipos, etc.
+  // Nos enfocamos en los componentes, servicios y lógica de negocio.
+  collectCoverageFrom: [
+    'src/app/components/**/*.{ts,tsx}',
+    // 'src/app/services/**/*.{ts,tsx}',
+    // 'src/app/hooks/**/*.{ts,tsx}',
+    '!src/app/**/*.d.ts',
+    '!src/app/**/layout.tsx',
+    '!src/app/**/page.tsx',
+    '!src/app/types/**',
+    '!src/app/context/**',
+    '!**/__tests__/**', // Excluimos con !
+  ],
+
+  // Mínimos de cobertura.
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
 };
 
-// createJestConfig es exportado de esta manera para asegurar que next/jest pueda cargar la configuración de Next.js
 export default createJestConfig(config);
